b571a82db7621128e57f8e2edf6d62c5c381d35c
gae gul <dkswgoud123@gmail.com>
2022-01-12 21:07:21 +0900

    2022.01.12

---
+++ 2_1_1_list_to_node.py
@@ -0,0 +1,51 @@
+class Node():
+    
+    def __init__(self,d=0,p=None, n=None):
+        self.data = d
+        self.prev = p
+        self.next = n
+
+class link_list:
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+# index  0  1  2  3  4
+# lst = [1, 2, 3, 4, 5]
+# i in 1,2,3,4,5
+
+def make_node(lst, N):    # N은 수열의 길이
+    first = last = Node(lst[0])
+    
+    for i in lst[1:]:
+        new = Node(i, last)
+        last.next = new
+        last = new
+    
+    if lst.head == None:    # 새로 만드는 노드 수열
+        lst.head, lst.tail = first, last
+    
+    else:
+        current = lst.head
+        
+        while current != None:
+            if current.data 
+
+
+
+
+
+
+
+
+
+
+
+# 3번의 테스트 리스트
+
+# 5 : 수열의 길이(N) / 2 : M회 추가(M) / 5 : L번 인덱스 출력(L)
+# 1 2 3 4 5
+# 2 : in_dex / 7 : num_ber
+# 4          / 8
