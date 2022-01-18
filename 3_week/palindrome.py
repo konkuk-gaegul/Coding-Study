58f34e2b74e8d27a62d3fdb368bde0c93dfccffe
gae gul <dkswgoud123@gmail.com>
2022-01-18 21:05:02 +0900

    2022.01.18

--- 3_week/palindrome.py
+++ 3_week/palindrome.py
@@ -1,6 +1,14 @@
 T = int(input())
 
 for i in range(1, T+1):
-    lists = list(input())
-    print( lists )
-    # for j in range(0, len(lists)):
+    lists = list(input())       # hello => 'h','e','l','l','o'
+    length = len(lists)         # length = 5
+    lists__ = []                # '__' 주의
+    
+    for j in range(length-1, -1, -1):
+        lists__.append(j)
+        
+    if lists == lists__:
+        print(1)
+    else:
+        print(0)
