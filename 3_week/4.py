c5bd97a409a0115e107a0749760e791d3e773d32
gae gul <dkswgoud123@gmail.com>
2022-01-20 19:50:30 +0900

    2022.01.20

--- 3_week/4.py
+++ 3_week/4.py
@@ -4,18 +4,23 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     Aj = list(map(int, input().split()))    # 1 5 3
     Bj = list(map(int, input().split()))    # 3 6 -7 5 4
-    a = len(Aj)     # Aj의 길이
-    b = len(Bj)     # Bj의 길이
-    Cj = []         # 넣고 비교할 빈문자열
-    cnt = 0
     
-    if a < b:
-        while cnt <= (a-b):
+    Cj = []         # 넣고 비교할 빈문자열
+    cnt = 0         # 하나씩 더해줄 곳
+    
+    if N < M:
+        while cnt <= (M-N):
             c = 0           # 하나씩 더해줄 곳
-            for i in range(a):
+            for i in range(N):
                 c += Aj[i]*Bj[i+cnt]
+            Cj.append(c)    # Cj에 c를 하나씩 넣어준다.
+            cnt += 1        # 넣어줄 때마다 카운트 +1
+        print(max( Cj ))    # 이렇게 모인 c들 중에서 가장 큰 것
+    else:                   # M이 N보다 작은 경우
+        while cnt <= (N-M):
+            c = 0
+            for j in range(M):
+                c += Bj[j]*Aj[j+cnt]
             Cj.append(c)
             cnt += 1
         print(max( Cj ))
-    # else:
-    #     while a >= b:
--- 3_week/palindrome.py
+++ 3_week/palindrome.py
@@ -3,10 +3,10 @@ T = int(input())
 for i in range(1, T+1):
     lists = list(input())       # hello => 'h','e','l','l','o'
     length = len(lists)         # length = 5
-    lists__ = []                # '__' 주의
+    lists__ = []                # '__' 주의, lists의 문자를 거꾸로 하나씩 넣음
     
     for j in range(length-1, -1, -1):
-        lists__.append(j)
+        lists__.append(lists[j])    # 'o', 'l', 'l', 'e', 'h'
         
     if lists == lists__:
         print(1)
