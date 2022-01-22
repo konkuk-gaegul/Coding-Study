9553ba2dee6a69ac87550b23d1c6dbaea187a929
gae gul <dkswgoud123@gmail.com>
2022-01-22 22:33:04 +0900

    2022.01.22

--- 4_week/Go_RC_Car.py
+++ 4_week/Go_RC_Car.py
@@ -7,23 +7,23 @@
 T = int(input())        # Test 횟수
 for tc in range(1, T+1):
     N = int(input())        # N개의 Command가 주어진다.
-    speed = 0
-    distance = 0
+    speed = 0               # 초기 속도 0
+    distance = 0            # 초기 이동거리 0
     
     for i in range(N):
-        a = list(map(int, input().split()))
+        a = list(map(int, input().split()))     # command 입력
         
-        if a[0] == 0:
-            distance += speed
+        if a[0] == 0:               # 현재 속도 유지
+            distance += speed       # 현재 속도의 단위 시간 이동거리
         
-        elif a[0] == 1:
-            speed = speed + a[1]
-            distance += speed
+        elif a[0] == 1:             # 가속의 경우
+            speed = speed + a[1]    # 현재 속도에 가속도*1초 더해주기
+            distance += speed       
             
-        elif a[0] == 2:
-            speed = speed - a[1]
-            if speed < 0:
-                speed = 0
+        elif a[0] == 2:             # 감속의 경우
+            speed = speed - a[1]    # 현재 속도에 가속도*1초 빼주기
+            if speed < 0:           # 속도가 0이하, 후진하는 경우
+                speed = 0           # 속도는 0으로 보정
             distance += speed
             
         
