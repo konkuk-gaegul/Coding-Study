3ffcdc9602e5ba9b6b7955dbb49d791c8aace912
gae gul <dkswgoud123@gmail.com>
2022-01-24 14:30:38 +0900

    2022.01.24

--- 3_week/palindrome2.py
+++ 3_week/palindrome2.py
@@ -11,15 +11,15 @@ for tc in range(1,11):        # 10회 테스트 반복
     # 가로(i) 방향
     for j in range(8):
         for i in range(8-N+1):      # 4글자 회문을 찾기위해 보정
-            A = List[j][i : i+N]    # 
+            A = List[j][i : i+N]    # i부터 (i+N-1)번 인덱스까지
             if A == A[::-1]:
                 Answer += 1
                 
     # 세로(j) 방향
     for i in range(8):
         for j in range(8-N+1):
-            A = ''              # j방향은 리스트가 아니라 인덱스 불가
-            for k in range(N):
+            A = ''                  # j방향은 리스트가 아니라 인덱스 불가
+            for k in range(N):      # 임시 저장소 A 생성
                 A += List[j+k][i]
             if A == A[::-1]:
                 Answer += 1
