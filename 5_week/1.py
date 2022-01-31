4ed3d0ac771b78bc40e12fe4b9a1c106e8482e32
gae gul <dkswgoud123@gmail.com>
2022-01-31 15:03:56 +0900

    2022.01.31

--- 5_week/1.py
+++ 5_week/1.py
@@ -7,4 +7,8 @@ for tc in range(1,T+1):
     N = int(input())
     matrix = [ list( map(int , input().split() )) for _ in range(N) ]
     
-    print(f'{matrix}')
+    for i in range(0,N):
+        for j in range(1-N,1):
+            matrix_90 = matrix[j][i]
+            
+    print(f'{matrix_90}')
