29eddd2527f28634cc86e6fd8d5e4deabca110e1
gae gul <dkswgoud123@gmail.com>
2022-01-31 16:34:06 +0900

    2022.01.31

--- 5_week/1.py
+++ 5_week/1.py
@@ -1,14 +1,25 @@
 # 숫자 배열 회전
 # NxN 행렬이 주어질 때
 # 시계 방향으로 90도, 180도, 270도 회전한 모양을 출력하라.
-
 T = int(input())
 for tc in range(1,T+1):
-    N = int(input())
-    matrix = [ list( map(int , input().split() )) for _ in range(N) ]
+    N = int(input())    # NxN행렬
+    matrix = [ list( map(int , input().split()) ) for _ in range(N) ]
     
-    for i in range(0,N):
-        for j in range(1-N,1):
-            matrix_90 = matrix[j][i]
-            
-    print(f'{matrix_90}')
+    mat_90 = [ [0 for _ in range(3)] for _ in range(3)]     # 0행렬 생성
+    mat_180 = [ [0 for _ in range(3)] for _ in range(3)]    # 0행렬 생성
+    mat_270 = [ [0 for _ in range(3)] for _ in range(3)]    # 0행렬 생성
+    
+    for i in range(N):
+        for j in range(N):
+            mat_90[i][j] = matrix[N-j-1][i]
+    
+    for i in range(N):
+        for j in range(N):
+            mat_180[i][j] = mat_90[N-j-1][i]
+    
+    for i in range(N):
+        for j in range(N):
+            mat_270[i][j] = mat_180[N-j-1][i]
+    
+    print(f'{mat_90} {mat_180} {mat_270}')
