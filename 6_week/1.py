19a88238255b3fc4db52bf10ef974a1c93fd11cc
gae gul <dkswgoud123@gmail.com>
2022-02-04 22:52:19 +0900

    2022.02.04

--- 6_week/1.py
+++ 6_week/1.py
@@ -1,9 +1,27 @@
-puzzle = [7, 3, 6, 4, 2, 9, 5, 8, 1]
-target = range(1,10)
-
-for i in range(1):
-    # puzzle_index = puzzle.index( puzzle[i] )
-    for j in range(10):
-        
-        if puzzle[i] == target[j]:
+def check(M):
+    for j in range(9):   # 열 방향
+        row_index = [0] * 10
+        col_index = [0] * 10
+        for i in range(9):  # 행 방향
             
+            # i방향 체크
+            row = M[j][i]
+            if row_index[row] == False:
+                row_index[row] = 1
+            else: return 0
+                
+            # j방향 체크
+            col = M[i][j]
+            if col_index[col] == False:
+                col_index[col] == 1
+            else: return 0
+            
+    return 1
+            
+T = int(input())
+for tc in range(1, T+1):
+    lst = [list(map(int, input().split())) for _ in range(9)]
+    
+    result = check(lst)
+    
+    print(f'#{tc} {result}')
