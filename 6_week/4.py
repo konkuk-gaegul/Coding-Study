c0876df579b792d30df6b8fd8448e94996ba735f
gae gul <dkswgoud123@gmail.com>
2022-02-07 22:14:19 +0900

    2022.02.07

---
+++ 6_week/4.py
@@ -0,0 +1,13 @@
+T = int(input())
+# a, b, c, d, e, f, g, h
+A = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+B = [0]*8
+for tc in range(1,T+1):
+    N = int(input())
+    
+    for i in range(len(A)):
+        a = N // A[i]   # a는 몫
+        B[i] = a
+        N = N % A[i]  # N에는 나머지를 다시 할당
+        
+    print(*B)   # 괄호와 컴마 없이 출력
