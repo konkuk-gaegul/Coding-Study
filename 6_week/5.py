08111151ffd3b24fd1b53ad992374356afb60390
gae gul <dkswgoud123@gmail.com>
2022-02-08 22:03:44 +0900

    2022.02.08

--- 6_week/5.py
+++ 6_week/5.py
@@ -2,16 +2,31 @@
 # 각 행, 열, 대각의 합 중
 # 가장 큰 값을 출력하라!
 
-for tc in range(10):
+for tc in range(1):
     # T = int(input())
     T = 1
-    # 연습삼아 5x5
-    N = 5
+    
+    # 연습삼아 3x3
+    N = 3
     matrix = [list(map(int, input().split())) for _ in range(N)]
-    a = []  # a에 각 행/열의 총 합을 넣음
-    
+    A = []
+    c = []
+    # 행의 합
     for j in range(N):
-        a = sum(matrix[j])
+        a = matrix[j]
+        A.append(sum(a))
+        
+    # 열의 합
+    for i in range(N):
+        b = matrix[j]
+        A.append(sum(b))
+        
+    # 대각 합
+    for j in range(N):
+        for i in range(j):
+            c.append(matrix[j][i])
+        A.append(sum(c))
+        
+    D = max(A, B, C)
     
-    
-print(f'#{T} {a}')
+print(f'#{T} {D}')
