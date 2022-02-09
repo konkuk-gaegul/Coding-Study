f47047d4e5090131b829b2d1868559a75b1a83d3
gae gul <dkswgoud123@gmail.com>
2022-02-09 23:34:45 +0900

    2022.02.09

--- 6_week/5.py
+++ 6_week/5.py
@@ -1,18 +1,19 @@
 # 입력되는 100x100 배열에서
 # 각 행, 열, 대각의 합 중
 # 가장 큰 값을 출력하라!
-
 for tc in range(1):
-    T = int(input())
-    N = 100
+    # T = int(input())
+    # N = 100
+    N = 3
     matrix = [list(map(int, input().split())) for _ in range(N)]
-    A = []
-    c = []
-    d = []
+    total = []  # 각 행/열/대각의 합을 나열
+    c = []  # 좌상->우하 대각
+    d = []  # 우상->좌하 대각
+    
     # 행의 합
     for j in range(N):
         a = sum(matrix[j])
-        A.append( a )
+        total.append( a )
         
     # 열의 합
     for i in range(N):
@@ -21,20 +22,20 @@ for tc in range(1):
         for j in range(N):
             b.append(matrix[j][i])
         col = sum(b)
-        A.append( col )
+        total.append( col )
         
     # 좌상->우하 대각
+    diag_1 = 0
     for j in range(N):
         for i in range(j,j+1):
-            c.append(matrix[j][i])
-    A.append(sum(c))
+            diag_1 += matrix[j][i]
+    total.append(sum(c))
     
     # 우상->좌하 대각
     for j in range(N):
         for i in range(-(j+1),-(j+2),-1):
             d.append(matrix[j][i])
-    A.append(sum(d))
+    total.append(sum(d))
     
-    D = max(A)
-    
-print(f'#{T} {D}')
+# print(f'# {max(total)}')
+print(f'# {diag_1}')
